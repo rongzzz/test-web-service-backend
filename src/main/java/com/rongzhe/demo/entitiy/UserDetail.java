c1215d2238087d049a5ef631a8a3561615bb1e3e
rongzzz <rongzhenick@gmail.com>
2021-03-18 18:17:19 +0800

    add user detail entity

---
+++ src/main/java/com/rongzhe/demo/entitiy/UserDetail.java
@@ -0,0 +1,47 @@
+package com.rongzhe.demo.entitiy;
+
+import javax.persistence.Access;
+import javax.persistence.AccessType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "sys_user_detail")
+@Access(AccessType.FIELD)
+public class UserDetail extends EntityBase {
+
+	@Column
+	private String firstName;
+
+	@Column
+	private String lastName;
+
+	@Column
+	private String displayName;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getDisplayName() {
+		return displayName;
+	}
+
+	public void setDisplayName(String displayName) {
+		this.displayName = displayName;
+	}
+
+}
